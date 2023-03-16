9ef7823782a426e68743bb7751be3e8bef25c2bd
Owais Rashid Mir <mirowais414@gmail.com>
2023-03-17 02:45:28 +0530

    Package program added.

---
+++ package1/src/myPack/myPackClass.java
@@ -0,0 +1,14 @@
+package myPack;
+
+public class myPackClass {
+
+	int a;
+	public void setValue(int z) {
+		a = z;
+	}
+	
+	public void display() {
+		System.out.println("Value of a is:" + a);
+	}
+
+}
---
+++ package1/src/myPackUsage.java
@@ -0,0 +1,10 @@
+import myPack.myPackClass;
+public class myPackUsage {
+
+	public static void main(String[] args) {
+		myPackClass obj = new myPackClass();
+		obj.setValue(10);
+		obj.display();
+	}
+
+}
